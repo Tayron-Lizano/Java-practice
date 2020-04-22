997f05ee8536a22eff9b97ba2dd406169971fedf
tayron <tp.lizano@gmail.com>
2020-04-22 10:21:25 -0500

    cambios: creacion de diferentes paquete, practica con modificadores de acceso

---
+++ POO/src/paquete/Clase3.java
@@ -0,0 +1,10 @@
+package paquete;
+
+import pruebas.Clase1;
+
+public class Clase3 extends Clase1{
+    public static void main(String []args){
+        Clase1 objeto =new Clase1();
+        objeto.
+    }
+}
---
+++ POO/src/pruebas/Clase2.java
@@ -0,0 +1,9 @@
+package pruebas;
+
+public class Clase2 {
+    public static void main(String []args){
+        Clase1 obj=new Clase1();
+        obj.
+    }
+
+}
