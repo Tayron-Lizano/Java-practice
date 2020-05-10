9c775e433a40c18a299701d668fa012785b698f9
Tayron <tp.lizano@gmail.com>
2020-05-10 10:03:28 -0500

    contrucción de Temporizador con Clase Timer del paquete java.swing.Timer

---
+++ POO/src/app/PruebaTemporizador.java
@@ -0,0 +1,28 @@
+package app;
+
+import javax.swing.JOptionPane;
+import javax.swing.Timer;
+import java.awt.event.*;
+import java.util.Date;
+
+
+public class PruebaTemporizador {
+    public static void main(String [] args){
+        ActionListener oyente = new DameLaHora();
+        Timer temporizador = new Timer(5000, oyente);
+
+        temporizador.start();
+
+        JOptionPane.showMessageDialog(null, "Pulsa aceptar para Deterner el Temporizador: ");
+        System.exit(0);
+
+    }
+}
+
+class DameLaHora  implements ActionListener{
+    public void actionPerformed(ActionEvent e){
+        Date ahora=new Date();
+        System.out.println("te devuelvo la hora cada 5 sg: "+ ahora);
+    }
+
+}
