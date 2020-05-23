05a96d7b31ee4673eff44b64c97a2490442b4824
tayron <tp.lizano@gmail.com>
2020-05-23 16:12:10 -0500

    writing on the frame using class JPanel

---
+++ POO/src/graficos/Maker.java
@@ -0,0 +1,34 @@
+package graficos;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Maker{
+    public static void main(String []args){
+        Writing window= new Writing();
+        window.setDefaultCloseOperation(3);
+    }
+}
+
+class Writing extends JFrame {
+    public Writing(){
+        Toolkit myToolkit= Toolkit.getDefaultToolkit();
+        Dimension mySreen = myToolkit.getScreenSize();
+        setBounds(mySreen.width/4, mySreen.height/4, mySreen.width/2, mySreen.height/2);
+        Lamina miLamina=new Lamina();
+        add(miLamina);
+        setVisible(true);
+        setTitle("My App");
+        Image icon= myToolkit.getImage("icon.png");
+        setIconImage(icon);
+        
+
+    }
+}
+
+class Lamina extends JPanel{
+    public void painComponent(Graphics g){
+        super.paintComponent(g);
+        g.drawString("Estamos escribiendo en el panel", 50, 50);
+    }
+}
