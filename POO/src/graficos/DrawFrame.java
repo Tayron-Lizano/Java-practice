a5e3904bb6302ec94ca99f33e4dbd152a564135e
tayron <tp.lizano@gmail.com>
2020-05-23 09:51:01 -0500

    building a centered frame using class Toolkit and class Dimension

---
+++ POO/src/graficos/DrawFrame.java
@@ -0,0 +1,27 @@
+package graficos;
+
+
+import java.awt.*;
+import javax.swing.JFrame;
+
+public class DrawFrame{
+    public static void main(String [] args){
+        MyFrame app= new MyFrame();
+
+    }
+}
+
+class MyFrame extends JFrame{
+    public MyFrame (){
+        Toolkit myScreen =Toolkit.getDefaultToolkit();
+        Dimension tamanio= myScreen.getScreenSize();
+        setBounds(tamanio.width/4, tamanio.height/4, tamanio.width/2, tamanio.height/2);
+        setVisible(true);
+        setTitle("My App");
+        Image icon = myScreen.getImage("src/graficos/icon.png");
+        setIconImage(icon);
+        setDefaultCloseOperation(3);
+        
+
+    } 
+}
