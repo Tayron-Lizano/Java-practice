3190f8394aa6f31689553c9b26bf2a5c8f40b397
tayron <tp.lizano@gmail.com>
2020-06-26 22:00:26 -0500

    working with Method Read(), class ImageIO

---
+++ POO/src/graficos/ImageMain.java
@@ -0,0 +1,65 @@
+package graficos;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.Toolkit;
+import java.awt.Graphics;
+import java.awt.Dimension;
+import java.awt.Image;
+import java.io.File;
+
+
+public class ImageMain{
+    public static void main(String [] args){
+        MakeFrameImage miMarco = new MakeFrameImage();
+        miMarco.setDefaultCloseOperation(3);
+
+    }
+}
+
+class MakeFrameImage extends JFrame{
+    public MakeFrameImage(){
+        Toolkit myToolkit = Toolkit.getDefaultToolkit();
+        myScreen = myToolkit.getScreenSize();
+        setBounds(myScreen.width/4, myScreen.height/4, myScreen.width/2, myScreen.height/2);
+        setVisible(true);
+        setTitle("My App");
+        Image icon = myToolkit.getImage("src/graficos/icon.png");
+        setIconImage(icon);
+
+        MakeLaminaImage lamina = new MakeLaminaImage();
+        add(lamina);
+    }
+    private Dimension myScreen;
+}
+
+class MakeLaminaImage extends JPanel{
+    public MakeLaminaImage(){
+        try {
+            File myImg = new File("src/graficos/icon.png");
+            imge = ImageIO.read(myImg);
+           
+        } catch (Exception e) {
+            //TODO: handle exception
+            System.out.println("la imagen no fue encontrada.");
+        }
+    }
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+
+        int ancho = imge.getWidth(this);
+        int alto = imge.getHeight(this);
+        g.drawImage(imge, 0, 0, null);
+        
+        for(int i =0;i<300;i++){
+            for(int j =0;j<300;j++){
+                if(i+j>0){
+                    g.copyArea(0, 0, ancho, alto, ancho*i, alto*j);
+                }
+            }
+        }
+    }
+    private Image imge;
+
+}
